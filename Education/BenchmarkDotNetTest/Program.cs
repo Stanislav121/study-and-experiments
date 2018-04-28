25ccc1d5b0130707250ab776e44647a16809ba6e
Stanislav121 <stanislav121511@yandex.ru>
2018-04-28 17:37:21 +0300

    Add test project with BenchmarkDotNet

---
+++ Education/BenchmarkDotNetTest/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace BenchmarkDotNetTest
+{
+    
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //var a = new Example();
+            //a.RunBenchmarks();
+
+            var a = new SymbolValidation();
+            a.RunBenchmarks();
+
+            Console.ReadLine();
+        }
+    }
+}
