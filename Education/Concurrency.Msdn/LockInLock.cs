7bfc6aa4904707ebeb7507840b648a1484e97a1f
stanislav12151 <stanislav121511@yandex.ru>
2019-03-21 22:18:01 +0300

    Add experiment with double lock on one sync object

---
+++ Education/Concurrency.Msdn/LockInLock.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Concurrency.Msdn
+{
+    class LockInLock
+    {
+        private readonly object _sync1 = new object();
+
+        private readonly object _sync2 = new object();
+
+        public void TestLockInLock()
+        {
+            lock(_sync1)
+                lock(_sync1)
+                {
+                    Console.WriteLine("Double lock on one sync object are through");
+                }
+        }
+
+        public void LongSyncOperation()
+        {
+            lock(_sync2)
+            {
+                Thread.Sleep(100000);
+            }
+        }
+
+    }
+}
--- Education/Concurrency.Msdn/Program.cs
+++ Education/Concurrency.Msdn/Program.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 
 namespace Concurrency.Msdn
 {
@@ -10,9 +10,25 @@ namespace Concurrency.Msdn
     {
         static void Main(string[] args)
         {
+            //RunWorkWithCancellationToken();
+            RunLockInLock();
+
+            Console.ReadLine();
+        }
+
+        private static void RunLockInLock()
+        {
+            var a = new LockInLock();
+            var thread = new Thread(a.LongSyncOperation);
+            thread.Start();
+            Thread.Sleep(100);
+            a.TestLockInLock();
+        }
+
+        private static void RunWorkWithCancellationToken()
+        {
             var a = new WorkWithCancellationToken();
             a.Run();
-            Console.ReadLine();
         }
     }
 }
