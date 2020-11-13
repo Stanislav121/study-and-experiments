e113601579d8b59f9f4da54fe884e41e06f53e6a
Stas B <Stanislav121511@yandex.ru>
2020-11-13 20:07:26 +0300

    Strings

---
+++ Education/Strings/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s1 = "Hello";
+            string s2 = " world";
+            string s3 = "Hello world";
+            string s5 = "Hello world";
+            string s4 = s1 + s2;
+
+            Console.WriteLine(s3 == s4); // True
+            Console.WriteLine(Object.ReferenceEquals(s3, s4));// False
+
+            Console.WriteLine(s3 == s5); // True
+            Console.WriteLine(Object.ReferenceEquals(s3, s5));// True
+        }
+    }
+}
